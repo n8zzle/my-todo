35a380f550f0939c2efd63891f5682eff03902c4
n8zzle <playwithn8zzle@gmail.com>
2022-12-20 01:31:34 +0100

    Thinked about personal data and how to actualize it

--- pages/ProfileForm.tsx
+++ pages/ProfileForm.tsx
@@ -1,16 +1,28 @@
+import { Avatar } from "@mui/material";
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import Menu from "../components/Menu";
 import Navbar from "../components/NavBar";
+import { auth } from "../configs/firebase.config";
 
 type Props = {};
 
 const ProfileForm = (props: Props) => {
+  const [user] = useAuthState(auth);
   return (
-    <div className="w-screen h-screen flex flex-row">
+    <div className="w-screen h-screen flex flex-row overflow-hidden">
       <Menu />
       <div className="w-full">
         <Navbar />
-        Profile Form
+        <div className="flex flex-col h-full justify-center items-center">
+          <h1 className="text-4xl font-extrabold tracking-[5px]">
+            Personal Information
+          </h1>
+          <div className="flex flex-col items-center  space-y-5 bg-gray-100 w-1/3 h-2/3 p-10 rounded-lg shadow-md">
+            <Avatar src={user.photoURL} sx={{ width: 200, height: 200 }} />
+            <p>{user.displayName}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
